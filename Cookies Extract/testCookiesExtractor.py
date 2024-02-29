53a8e2d86823144f62df7f50a658787e5b4965aa
tensazangesto98754 <tensazangesto98754@gmail.com>
2024-02-29 21:52:10 +0330

    create two function to decode and get date time of chrome

--- Cookies Extract/testCookiesExtractor.py
+++ Cookies Extract/testCookiesExtractor.py
@@ -1,11 +1,11 @@
-import os
-import json
 import base64
-import sqlite3
-import shutil
+import json
+import os
 from datetime import datetime, timedelta
 import win32crypt
 from Crypto.Cipher import AES
+import shutil
+import sqlite3
 
 
 def getChromeDateTime(ChromDate):
@@ -17,3 +17,38 @@ def getChromeDateTime(ChromDate):
             return ChromDate
     else:
         return " "
+
+
+def getEncyptionKey():
+    LocalStatePath = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data",
+                                  "Local State")
+    with open(LocalStatePath, "r", encoding="utf-8") as f:
+        LocalState = f.read()
+        LocalState = json.loads(LocalState)
+
+    Key = base64.b64decode(LocalState["os_crypt"]["encrypted_key"])
+
+    Key = Key[5:]
+    return win32crypt.CryptUnprotectData(Key, None, None, None, 0)[1]
+
+
+def decryptData(Data, Key):
+    try:
+        fileIv = Data[3:15]
+        Data = Data[15:]
+
+        chiperObj = AES.new(Key, AES.MODE_GCM, fileIv)
+
+        return chiperObj.decrypt(Data)[:-16].decode()
+    except:
+        try:
+            return str(win32crypt.CryptUnprotectData(Data, None, None, None, 0)[1])
+        except:
+            return " "
+
+
+
+
+
+
+
