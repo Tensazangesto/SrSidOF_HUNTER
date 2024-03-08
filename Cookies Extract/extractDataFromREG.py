b29ffdd4a6b6ba3263ee2e6f9c2576e221240033
tensazangesto98754 <tensazangesto98754@gmail.com>
2024-03-08 09:10:32 +0330

    after searching for day I found way to export

--- Cookies Extract/extractDataFromREG.py
+++ Cookies Extract/extractDataFromREG.py
@@ -1,6 +1,8 @@
-from _winapi import GetCurrentProcess
-from winreg import *
-from win32security import *
-with OpenKey(HKEY_CURRENT_USER, r'Software\Google') as key:
-    AdjustTokenPrivileges(OpenProcessToken(GetCurrentProcess(), 40), 0, [LookupPrivilegeValue(None, 'SeDebugPrivilege')], 2)
-    SaveKey(key, 'C:\\Users\\Javad\\Desktop\\test')
+from os import system, environ
+# define function that get data from registry by bath script
+def getData():
+    system("reg export HKEY_CURRENT_USER\Software\Google\Chrome\PreferenceMACs C:\Web\export.txt")
+
+
+
+getData()
