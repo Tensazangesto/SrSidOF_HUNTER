7a742f926bcad2f9573e2b30fa106cdd6779c0cb
ehsan mehranmoghadam <ehsanmehram361@gmail.com>
2024-06-02 21:38:14 +0330

    update installer and reset

--- MoreTime/reset.py
+++ MoreTime/reset.py
@@ -1,15 +1,17 @@
 from os import system, environ, startfile
 from os.path import exists
 from time import sleep
+from library_data import MoreTime_code, Performer_code
 
 
 try:
-    path_MoreTime_1 = environ['APPDATA'] + r'\MoreTime\MoreTime.exe'
-    path_MoreTime_1_2 = environ['APPDATA'] + r'\MoreTime\Performer.bat'
-    path_MoreTime_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\Startup\Performer.bat'
-    path_MoreTime_2_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\MoreTime.exe'
+    path_MoreTime_1 = environ['APPDATA'] + r'\MoreTime\MoreTime.exe'#Admin
+    path_MoreTime_1_2 = environ['APPDATA'] + r'\MoreTime\Performer.bat'#Admin ranner
+    path_MoreTime_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\Startup\Performer.bat'#User
+    path_MoreTime_2_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\MoreTime.exe'#User ranner
+    destination = environ['APPDATA'] + "\\Microsoft\\Windows\\Start Menu\\Programs"
     while True:
-        sleep(5)#time stop
+        sleep(1200)#time stop
         system("taskkill /F /im MoreTime.exe")
         sleep(3)
         if(exists(path_MoreTime_1) and exists(path_MoreTime_1_2)):
@@ -17,6 +19,9 @@ try:
         elif(exists(path_MoreTime_2) and exists(path_MoreTime_2_2)):
             startfile(path_MoreTime_2)
         else:
-            break
+            with open(file=destination + "\\MoreTime.exe", mode='wb') as f:
+                f.write(MoreTime_code())
+            with open(file=destination + "\\Startup\\Performer.bat", mode="w", encoding="utf-8") as f:
+                f.write(Performer_code())
 except:
     pass
