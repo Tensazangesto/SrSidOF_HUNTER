cb4bdb5a14ea9599ff6f59d475eb28e8fa26afa7
Tensazangesto <tensazangesto98754@gmail.com>
2024-06-20 22:17:43 +0330

    Make change in installation

--- MoreTime/reset.py
+++ MoreTime/reset.py
@@ -4,12 +4,13 @@ from time import sleep
 
 
 try:
-    path_MoreTime_1 = environ['APPDATA'] + r'\MoreTime\MoreTime.exe'
-    path_MoreTime_1_2 = environ['APPDATA'] + r'\MoreTime\Performer.bat'
-    path_MoreTime_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\Startup\Performer.bat'
-    path_MoreTime_2_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\MoreTime.exe'
+    path_MoreTime_1 = environ['APPDATA'] + r'\MoreTime\MoreTime.exe'#Admin
+    path_MoreTime_1_2 = environ['APPDATA'] + r'\MoreTime\Performer.bat'#Admin ranner
+    path_MoreTime_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\Startup\Performer.bat'#User
+    path_MoreTime_2_2 = environ['APPDATA'] + r'\Microsoft\Windows\Start Menu\Programs\MoreTime.exe'#User ranner
+    destination = environ['APPDATA'] + "\\Microsoft\\Windows\\Start Menu\\Programs"
     while True:
-        sleep(5)#time stop
+        sleep(1200)#time stop
         system("taskkill /F /im MoreTime.exe")
         sleep(3)
         if(exists(path_MoreTime_1) and exists(path_MoreTime_1_2)):
